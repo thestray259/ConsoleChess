76ea809baec3d927668e5a41488009e63a8af4f1
Tobie Allen <tallen@student.neumont.edu>
2023-01-09 16:36:57 -0700

    Test methods added still don't know what I'm gonna be testing tho

--- Test/ChessTests.cs
+++ Test/ChessTests.cs
@@ -6,8 +6,29 @@ namespace Test
     public class ChessTests
     {
         [TestMethod]
-        public void KingBetweenRooks()
+        public void Test1()
         {
+
+        }
+
+        public void Test2()
+        {
+
+        }
+
+        public void Test3()
+        {
+
+        }
+
+        public void Test4()
+        {
+
+        }
+
+        public void Test5()
+        {
+
         }
     }
 }
